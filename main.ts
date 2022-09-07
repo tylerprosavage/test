5273d09ddee3de49ee2fb8c5c15231552c0028f0
tylerprosavage <113040142+tylerprosavage@users.noreply.github.com>
2022-09-07 11:10:31 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+let hui = 0
+if (0 == hui) {
+    for (let index = 0; index < 10; index++) {
+        basic.showString("Hello!")
+    }
+} else {
+    basic.showNumber(50 - 985)
+}
+for (let index = 0; index < 4; index++) {
+    basic.showNumber(10)
+}
+basic.forever(function () {
+	
+})
